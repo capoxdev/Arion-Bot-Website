cb36ad6918cc283c8cb291478faf19d2ed4cc4da
capoxdev <capoxfleet@outlook.com>
2024-11-10 20:16:00 +0300

    Update Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -105,7 +105,7 @@ export default function Footer() {
                                 </p>
                         </div>
                         <p className="text-white text-center sm:text-right text-opacity-50">
-                            {"Powered by Nodesty ❤️"}
+                            {"Powered by capoxdev ❤️"}
                         </p>
                     </div>
                 </div>
